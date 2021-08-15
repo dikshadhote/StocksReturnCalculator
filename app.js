ca7d03d82360b03c6935056f71d755cc97536702
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-08-15 23:18:18 +0530

    updated all features of profit loss calculator

---
+++ app.js
@@ -0,0 +1,57 @@
+let initialPrice=document.querySelector('#initial-price');
+let quantityStocks=document.getElementById('quantity-stocks');
+let currentPrice=document.getElementById('current-price');
+
+let submitButton=document.getElementById('submit-btn');
+let output=document.getElementById('output');
+
+submitButton.addEventListener('click',onClickHandler);
+
+function onClickHandler()
+{
+    let initialP=parseInt(initialPrice.value);
+    let quantity=parseInt(quantityStocks.value);
+    let currentP=parseInt(currentPrice.value);
+    calculateProfitOrLoss(initialP,quantity,currentP);
+}
+
+
+function calculateProfitOrLoss(initialP,quantity,currentP)
+{
+    let profitLoss=0,PercentageProfitLoss=0;
+    
+        if(currentP >initialP)
+        {
+            //profit
+            profitLoss=(currentP-initialP)*quantity;
+            PercentageProfitLoss=(profitLoss*100)/initialP;
+            document.getElementById('output').style.color="lawngreen";
+            displayMessage(`Hey! you just earned a profit of ${profitLoss} with ${PercentageProfitLoss}%`);
+          
+
+        }
+        else if(initialP >currentP)
+        {
+            //loss
+            profitLoss=(currentP-initialP)*quantity;
+            PercentageProfitLoss=((profitLoss*100)/initialP);
+            document.getElementById('output').style.color="rgb(255, 117, 26)";
+            displayMessage(`Hey! you have to bear loss of ${profitLoss} with ${PercentageProfitLoss}%`);
+            
+
+        }
+        else{
+            //neither profit nor loss 
+            document.getElementById('output').style.color="Aqua";
+            displayMessage("If you can't see something moving, doesn't mean its not going somewhere!");
+        }
+
+}
+
+function displayMessage(message)
+{
+    output.innerHTML=message;
+}
+
+
+
